e6a54d010c728b348a9eaab9d889cdd8aab729bf
joaoleocadio <joaocarvalho388@gmail.com>
2020-03-22 14:54:28 +0000

    done

---
+++ Ficha03/Ex04/src/ex04/Authors.java
@@ -0,0 +1,8 @@
+package ex04;
+
+public class Authors {
+    private final int letras = 3;
+    public char[] Acro = new char[letras];
+    public String Nome;
+    public String Nacionalidade;
+}
---
+++ PP_FP04/src/pp_fp04/exchange/CurrencyRates.java
@@ -0,0 +1,23 @@
+package pp_fp04.exchange;
+
+import pp_fp04.Expenses;
+
+
+public class CurrencyRates {
+    public static double[][] TaxMatrix = new double[][]{{1.00000, 0.76900, 96.1141, 0.67131},
+    {1.13, 1.00000, 124.989, 0.87290},
+    {96.06148, 125.00000, 1.00000, 143.26648}};
+    
+    private static double ValorDolar = 0;
+    private static double ValorIene = 0;
+    private static double ValorLibra = 0;
+    private static double MediaDolar = 0;
+    private static double MediaIene = 0;
+    private static double MediaLibra = 0;
+    public Expenses despesa;
+
+    
+    
+    
+    
+}
